ef319180ddd043587890cd3f0d47a8cc97abbc6c
chungchihhan <harryup2000@gmail.com>
2024-08-21 20:22:43 +0800

    style: change the style of the result

--- src/ui/resultCard.tsx
+++ src/ui/resultCard.tsx
@@ -61,23 +61,40 @@ export default function ResultCard({
           />
         </div>
       </div>
-      <div className="flex justify-center pl-5 pr-3 pb-2 text-wrap">
-        <p className="font-cubic text-sm">
-          &quot;適應性強且靈活的人，能夠根據環境的變化迅速調整自己的步伐。擁有強大的能力，可以在需要的時候全力以赴，也可以在平靜時輕鬆應對。&quot;
-        </p>
+      <div className="flex justify-evenly pb-4">
+        <div>
+          <p className="font-cubic text-lg text-[#23303F] py-2">靈魂伴侶</p>
+          <div className="flex items-center gap-2">
+            <img src="/services/IAM.png" alt="IAM" className="w-12 h-12" />
+            <p className="font-cubic">IAM</p>
+          </div>
+        </div>
+        <div>
+          <p className="font-cubic text-lg text-[#23303F] py-2">偶爾一起玩</p>
+          <div className="flex items-center gap-2">
+            <img src="/services/ELB.png" alt="ELB" className="w-12 h-12" />
+            <p className="font-cubic">ELB</p>
+          </div>
+        </div>
       </div>
       <hr className="border-y-2 border-black my-2" />
-      <div className="pl-5 pr-3 text-wrap">
-        <p className="font-cubic text-md text-[#23303F] py-2">
-          在職場上的你...
-        </p>
-        <p className="font-cubic text-sm text-wrap w-full">
-          能迅速掌握新技能，並根據工作需求快速調整自己的步伐。他們在面對高強度的工作壓力時，能夠全力以赴，展現出卓越的表現和效率。無論是處理複雜的項目還是應對緊急情況，他們總是能夠迎刃而解。
-        </p>
+      <div className="flex p-4 gap-2">
+        <div className="flex flex-col text-wrap w-1/2 gap-2">
+          <p className="font-cubic text-lg text-[#23303F]">在職場上的你...</p>
+          <p className="font-cubic text-sm text-wrap w-full">
+            能迅速掌握新技能，並根據工作需求快速調整自己的步伐。他們在面對高強度的工作壓力時，能夠全力以赴，展現出卓越的表現和效率。無論是處理複雜的項目還是應對緊急情況，他們總是能夠迎刃而解。
+          </p>
+        </div>
+        <div className="flex flex-col text-wrap w-1/2 gap-2">
+          <p className="font-cubic text-lg text-[#23303F]">在生活上的你...</p>
+          <p className="font-cubic text-sm text-wrap w-full">
+            適應性強且靈活的人，能夠根據環境的變化迅速調整自己的步伐。擁有強大的能力，可以在需要的時候全力以赴，也可以在平靜時輕鬆應對。
+          </p>
+        </div>
       </div>
       <hr className="border-y-2 border-black my-2" />
       <div className="pl-5 pr-3 pb-5 text-wrap">
-        <p className="font-cubic text-md text-[#23303F] py-2">AWS 服務小教室</p>
+        <p className="font-cubic text-lg text-[#23303F] py-2">AWS 服務小教室</p>
         <p className="font-cubic text-sm">
           提供可擴展計算能力的服務。用戶可以在雲中快速啟動虛擬機器，選擇不同的作業系統和配置。這對於處理不同的工作負載，例如應用程式伺服器、資料庫或後端伺服器等都非常有用
         </p>
