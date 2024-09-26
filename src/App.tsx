587bc7bd82e758f6b5159fd5eb36e1e6c1824231
chungchihhan <harryup2000@gmail.com>
2024-09-26 20:32:30 +0800

    fix: uncomment the generate image

--- src/App.tsx
+++ src/App.tsx
@@ -118,7 +118,7 @@ export default function App() {
           prompt: prompt,
         },
       };
-      // generateImage(requestBody);
+      generateImage(requestBody);
     }
   }, [answerService]);
 
